df46ca2a9d18f91cbe261ee33c340031c9945a8b
Ouassim Behlil <bahlil2001@gmail.com>
2026-06-03 20:21:47 +0100

    expose data classes as module

---
+++ flyin/model/__init__.py
@@ -0,0 +1,12 @@
+from .connection import Connection
+from .drone import Drone
+from .metadata import ZoneType, Metadata
+from .zone import Zone
+
+__all__ = [
+    'Connection',
+    'Drone',
+    'ZoneType',
+    'Metadata',
+    'Zone'
+]
